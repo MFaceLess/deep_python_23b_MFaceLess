9cee551397a182a57bebef416e4fd7d6443ed81a
Michail Gilev <mishagilevfgh@e1.ru>
2023-10-15 13:06:18 +0300

    Fix Note From Lector

--- 04/test_descriptors.py
+++ 04/test_descriptors.py
@@ -58,6 +58,41 @@ class TestCustomList(unittest.TestCase):
             "e4 e5 kf3 kf6 ke5 d6 kf3 ke4",
             my_prepare.rus_opening)
 
+    def test_not_changing_value(self):
+        my_prepare = descriptors.ChessOpeningsPrepare(
+            "e4 e5 kf3 kf6", "e4 c5", "e4 e6")
+        with self.assertRaises(ValueError) as err:
+            my_prepare.rus_opening = "e4 e5 kf3 kc6"
+        self.assertEqual("wrong opening", str(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            my_prepare.sicilian_opening = "e4 b6"
+        self.assertEqual("wrong opening", str(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            my_prepare.fr_opening = "e4 b6"
+        self.assertEqual("wrong opening", str(err.exception))
+
+        self.assertEqual("e4 e5 kf3 kf6", my_prepare.rus_opening)
+        self.assertEqual("e4 c5", my_prepare.sicilian_opening)
+        self.assertEqual("e4 e6", my_prepare.fr_opening)
+
+        with self.assertRaises(ValueError) as err:
+            my_prepare.rus_opening = 15
+        self.assertEqual("string notation required", str(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            my_prepare.sicilian_opening = 15
+        self.assertEqual("string notation required", str(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            my_prepare.fr_opening = 15
+        self.assertEqual("string notation required", str(err.exception))
+
+        self.assertEqual("e4 e5 kf3 kf6", my_prepare.rus_opening)
+        self.assertEqual("e4 c5", my_prepare.sicilian_opening)
+        self.assertEqual("e4 e6", my_prepare.fr_opening)
+
 
 if __name__ == '__main__':
     unittest.main()
